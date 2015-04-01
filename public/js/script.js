1ca8c7451349cb64b9012be3f54c545b74e6dee7
Rodrigo Raya <roraya22@gamil.com>
2015-03-31 18:32:05 -0600

    It was added a pagination bar.

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
 // Code goes here
 var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
 
-function MyController($scope) {
+myApp.controller('MyController', function MyController($scope) {
   $scope.currentPage = 1;
   $scope.pageSize = 10;
   $scope.users = [];
-
-  $scope.hello = 'Rodrigo Raya';
+  $scope.totalPages = 0;
+  $scope.page_list = [];
 
   var names = [
     'Pedro',
@@ -81,23 +81,86 @@ function MyController($scope) {
     var last_name = surnames[Math.floor(Math.random() * surnames.length)];
 
     $scope.users.push({
-      id: '00' + i,
+      id: i,
       name: name + ' ' + last_name,
       amount: (Math.random() + 100) * (Math.random() * (Math.random() * 1000)),
       job: jobs[Math.floor(Math.random() * jobs.length)]
     });
   }
 
+  $scope.pages = function() {
+    $scope.totalPages = Math.ceil($scope.users.length / $scope.pageSize);
+
+    $scope.selectCurrentPage();
+  };
+
+  $scope.prevPage = function() {
+    $scope.currentPage -= 1;
+    $scope.selectCurrentPage();
+  };
+
+  $scope.selectPage = function(n) {
+    $scope.currentPage = n;
+    $scope.selectCurrentPage();
+  };
+
+  $scope.nextPage = function() {
+    $scope.currentPage += 1;
+    $scope.selectCurrentPage();
+  };
+
+  $scope.selectCurrentPage = function() {
+    var pages = $scope.totalPages;
+    var page = $scope.currentPage;
+    $scope.page_list = [];
+
+    if (pages > 6) {
+
+
+      if (page == pages) {
+        page -= 4;
+
+        $scope.page_list.push(1);
+        $scope.page_list.push('...');
+      }
+      else if (page < (pages - 3) && page > 1)
+        page -= 1;
+      else if (page > (pages - 3)) {
+        page = pages - 4;
+
+        $scope.page_list.push(1);
+        $scope.page_list.push('...');
+      }
+
+      for (var i = 0; i < 4; i++) {
+        $scope.page_list.push(page + i);
+      }
+
+      if (page < pages && page < (pages - 4))
+        $scope.page_list.push('...');
+
+      $scope.page_list.push($scope.totalPages);
+    }
+    else
+      for (var i = 1; i <= $scope.totalPages; i++) {
+        $scope.page_list.push(i);
+      }
+
+  };
+
   $scope.pageChangeHandler = function(num) {
       console.log('users page changed to ' + num);
   };
-}
 
-function OtherController($scope) {
-  $scope.pageChangeHandler = function(num) {
-    console.log('going to page ' + num);
-  };
-}
+  $scope.pages();
+});
 
-myApp.controller('MyController', MyController);
-myApp.controller('OtherController', OtherController);
+
+/************************************************/
+// function OtherController($scope) {
+//   $scope.pageChangeHandler = function(num) {
+//     console.log('going to page ' + num);
+//   };
+// }
+
+// myApp.controller('OtherController', OtherController);
